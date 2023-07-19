701d25913068f87b9000dfb376d33f70e7737566
Phelipe Maciel <135077679+Phelipe18@users.noreply.github.com>
2023-07-19 20:26:25 -0300

    Update 3_visao_restaurantes.py

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -7,7 +7,7 @@ from streamlit_folium import folium_static
 import folium
 import plotly.graph_objects as go
 
-df = pd.read_csv( 'dataset/train.csv' )
+df = pd.read_csv( '..dataset/train.csv' )
 
 # Limpeza de arquivo
 linhas_vazias = df['ID'] != 'NaN '
